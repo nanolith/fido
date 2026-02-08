a6efac3d1dc937fd12457430bce1a510e77e5266
Justin Handville <nanolith@gmail.com>
2026-02-08 01:44:33 -0500

    FIDO_SCANNER_TOKEN_TYPE_ROLE decl.

---
+++ include/fido/scanner.h
@@ -0,0 +1,29 @@
+/**
+ * \file fido/scanner.h
+ *
+ * \brief Scanner for fido.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+/**
+ * \brief Token types.
+ */
+enum fido_scanner_token_type
+{
+    /** \brief Role keyword. */
+    FIDO_SCANNER_TOKEN_TYPE_ROLE = 1,
+};
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
