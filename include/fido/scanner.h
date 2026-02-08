8d26469ad2b528de6ccfc79ef4007889d348a95b
Justin Handville <nanolith@gmail.com>
2026-02-08 02:54:23 -0500

    FIDO_SCANNER_TOKEN_TYPE_USERNAME decl.

--- include/fido/scanner.h
+++ include/fido/scanner.h
@@ -37,8 +37,10 @@ enum fido_scanner_token_type
     FIDO_SCANNER_TOKEN_TYPE_CLOSE_BRACE,
     /** \brief String. */
     FIDO_SCANNER_TOKEN_TYPE_STRING,
-    /** \brief Add environment variable.. */
+    /** \brief Add environment variable. */
     FIDO_SCANNER_TOKEN_TYPE_ADD_VARIABLE,
+    /** \brief Username. */
+    FIDO_SCANNER_TOKEN_TYPE_USERNAME,
 };
 
 /* C++ compatibility. */
