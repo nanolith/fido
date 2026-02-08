03cb8251cafebd67ff9b2ad4b25343e521419d95
Justin Handville <nanolith@gmail.com>
2026-02-08 17:02:09 -0500

    fido_scanner_read_token_keyword_role decl.

--- include/fido/scanner.h
+++ include/fido/scanner.h
@@ -279,6 +279,28 @@ MODEL_CONTRACT_POSTCONDITIONS_BEGIN(
         }
 MODEL_CONTRACT_POSTCONDITIONS_END(fido_scanner_peek_token)
 
+/**
+ * \brief Attempt to read a role keyword token.
+ *
+ * \note This function exists mainly for verification purposes. The function,
+ * \ref fido_scanner_complete_token_keyword_role is used by the scanner proper,
+ * and called by this function.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - FIDO_SCANNER_TOKEN_TYPE_KEYWORD_ROLE on success.
+ *      - FIDO_SCANNER_TOKEN_TYPE_BAD_INPUT if the scanner encounteres bad
+ *        input.
+ *      - FIDO_SCANNER_TOKEN_TYPE_EOF if the scanner encounters an EOF
+ *        condition.
+ */
+int FN_DECL_MUST_CHECK
+fido_scanner_read_token_keyword_role(
+    fido_token_details* details, fido_scanner* scanner);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
