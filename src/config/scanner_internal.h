712f3a6ab7bb4292bc9f6e41154003cb2bdbfd7a
Justin Handville <nanolith@gmail.com>
2026-02-08 17:09:12 -0500

    fido_scanner_complete_token_keyword_role decl.

---
+++ src/config/scanner_internal.h
@@ -0,0 +1,44 @@
+/**
+ * \file config/scanner_internal.h
+ *
+ * \brief Internal functions and types for the fido scanner.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <fido/scanner.h>
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+/******************************************************************************/
+/* Start of private methods.                                                  */
+/******************************************************************************/
+
+/**
+ * \brief Attempt to complete a role keyword token.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - FIDO_SCANNER_TOKEN_TYPE_KEYWORD_ROLE on success.
+ *      - FIDO_SCANNER_TOKEN_TYPE_BAD_INPUT if the scanner encounteres bad
+ *        input.
+ *      - FIDO_SCANNER_TOKEN_TYPE_EOF if the scanner encounters an EOF
+ *        condition.
+ */
+int FN_DECL_MUST_CHECK
+fido_scanner_complete_token_keyword_role(
+    fido_token_details* details, fido_scanner* scanner);
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
