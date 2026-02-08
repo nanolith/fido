96422fe97953a46616cba64d3c7e1beefe13d824
Justin Handville <nanolith@gmail.com>
2026-02-08 04:17:38 -0500

    fido_scanner_read_token decl.

--- include/fido/scanner.h
+++ include/fido/scanner.h
@@ -169,6 +169,28 @@ MODEL_CONTRACT_POSTCONDITIONS_BEGIN(
     fido_scanner_release, fido_scanner* scanner)
 MODEL_CONTRACT_POSTCONDITIONS_END(fido_scanner_release)
 
+/******************************************************************************/
+/* Start of public methods.                                                   */
+/******************************************************************************/
+
+/**
+ * \brief Read a token from the scanner instance, populating the provided token
+ * structure with details.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - FIDO_SCANNER_TOKEN_TYPE_EOF at the end of input.
+ *      - FIDO_SCANNER_TOKEN_TYPE_BAD_INPUT if the scanner encounteres bad
+ *        input.
+ *      - a value belonging to the \ref fido_scanner_token_type enumeration.
+ */
+int FN_DECL_MUST_CHECK
+fido_scanner_read_token(
+    fido_token_details* details, fido_scanner* scanner);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
