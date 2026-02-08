60e28ed4daecf4fdd5b84718d079b740a3195a48
Justin Handville <nanolith@gmail.com>
2026-02-08 16:27:00 -0500

    property_fido_scanner_valid impl.

---
+++ models/shadow/config/property_fido_scanner_valid.c
@@ -0,0 +1,31 @@
+/**
+ * \file models/shadow/config/property_fido_scanner_valid.c
+ *
+ * \brief Model checking property to ensure that a scanner instance is valid.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <fido/scanner.h>
+
+/**
+ * \brief Valid scanner property.
+ *
+ * \param alloc         The scanner instance to be verified.
+ *
+ * \returns true if the scanner instance is valid.
+ */
+bool
+property_fido_scanner_valid(
+    const fido_scanner* scanner)
+{
+    MODEL_CHECK_OBJECT_READ(scanner, sizeof(*scanner));
+    MODEL_CHECK_OBJECT_READ(scanner->input + scanner->index, 1);
+    MODEL_ASSERT(NULL != scanner->original_input);
+    MODEL_ASSERT(NULL != scanner->input);
+    MODEL_ASSERT(scanner->line >= 1);
+    MODEL_ASSERT(scanner->col >= 1);
+
+    return true;
+}
