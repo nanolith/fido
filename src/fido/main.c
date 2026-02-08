dd3a2669ed62e89ddd46cae28b2489a4d59345d3
Justin Handville <nanolith@gmail.com>
2026-02-08 00:46:22 -0500

    Added fido stub main.

---
+++ src/fido/main.c
@@ -0,0 +1,28 @@
+/**
+ * \file fido/main.c
+ *
+ * \brief Entry point for fido.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <stdio.h>
+
+/**
+ * \brief Entry point for fido.
+ *
+ * \param argc          Argument count.
+ * \param argv          Arguments.
+ *
+ * \returns 0 on success an non-zero on error.
+ */
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    printf("Not yet implemented.\n");
+
+    return 1;
+}
