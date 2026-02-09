d0781568c1a1c83dd58c912cb2b5d5b8532fa844
Justin Handville <nanolith@gmail.com>
2026-02-09 02:57:12 -0500

    fido_scanner_read_token_keyword_cmd impl.

---
+++ src/config/fido_scanner_read_token_keyword_cmd.c
@@ -0,0 +1,62 @@
+/**
+ * \file config/fido_scanner_read_token_keyword_cmd.c
+ *
+ * \brief Attempt to scan a cmd keyword, falling back to username if 'c'
+ * matches, and failing if it does not.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <ctype.h>
+
+#include "scanner_internal.h"
+
+/**
+ * \brief Attempt to read a cmd keyword token.
+ *
+ * \note This function exists mainly for verification purposes. The function,
+ * \ref fido_scanner_complete_token_keyword_cmd is used by the scanner proper,
+ * and called by this function.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - FIDO_SCANNER_TOKEN_TYPE_KEYWORD_CMD or
+ *        FIDO_SCANNER_TOKEN_TYPE_USERNAME on success.
+ *      - FIDO_SCANNER_TOKEN_TYPE_BAD_INPUT if the scanner encounteres bad
+ *        input.
+ */
+int FN_DECL_MUST_CHECK
+fido_scanner_read_token_keyword_cmd(
+    fido_token_details* details, fido_scanner* scanner)
+{
+    int retval;
+
+    MODEL_CONTRACT_CHECK_PRECONDITIONS(
+        fido_scanner_read_token_keyword_cmd, details, scanner);
+
+    /* for this function, the token must begin with 'c'. */
+    if ('c' != *(scanner->input))
+    {
+        goto bad_input;
+    }
+
+    /* start the token. */
+    fido_scanner_token_details_begin(details, scanner);
+
+    /* attempt to complete this token. */
+    retval = fido_scanner_complete_token_keyword_cmd(details, scanner);
+    goto done;
+
+bad_input:
+    retval = FIDO_SCANNER_TOKEN_TYPE_BAD_INPUT;
+
+done:
+    MODEL_CONTRACT_CHECK_POSTCONDITIONS(
+        fido_scanner_read_token_keyword_cmd, retval, details, scanner);
+
+    return retval;
+}
