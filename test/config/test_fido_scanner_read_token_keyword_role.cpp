e5dfb8b4906bf40984110a21d6349cec98941bb3
Justin Handville <nanolith@gmail.com>
2026-02-09 02:27:58 -0500

    Test that we can scan "role" as the role keyword.

---
+++ test/config/test_fido_scanner_read_token_keyword_role.cpp
@@ -0,0 +1,43 @@
+/**
+ * \file test/config/test_fido_scanner_read_token_keyword_role.c
+ *
+ * \brief Test that we can read the "role" keyword.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <fido/scanner.h>
+#include <minunit/minunit.h>
+
+TEST_SUITE(fido_scanner_read_token_keyword_role);
+
+/**
+ * \brief Basic test of the happy path.
+ */
+TEST(basics)
+{
+    fido_scanner* scanner = nullptr;
+    fido_token_details details;
+    const char* TEST_INPUT = "role";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == fido_scanner_create(&scanner, TEST_INPUT));
+
+    /* read the token keyword. */
+    TEST_ASSERT(
+        FIDO_SCANNER_TOKEN_TYPE_KEYWORD_ROLE ==
+            fido_scanner_read_token_keyword_role(&details, scanner));
+
+    /* the details are correct. */
+    TEST_EXPECT(FIDO_SCANNER_TOKEN_TYPE_KEYWORD_ROLE == details.type);
+    TEST_EXPECT(0 == details.begin_index);
+    TEST_EXPECT(3 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(1 == details.begin_col);
+    TEST_EXPECT(4 == details.end_col);
+
+    /* clean up. */
+    fido_scanner_release(scanner);
+}
