9f30fd7b098c84cf28da499a02cc067c1841ce4c
Justin Handville <nanolith@gmail.com>
2026-02-08 20:05:28 -0500

    fido_scanner_next_character function contract preconditions.

--- src/config/scanner_internal.h
+++ src/config/scanner_internal.h
@@ -27,6 +27,15 @@ extern "C" {
  */
 void fido_scanner_next_character(fido_scanner* scanner);
 
+/* function contract preconditions. */
+MODEL_CONTRACT_PRECONDITIONS_BEGIN(
+    fido_scanner_next_character, fido_scanner* scanner)
+        /* scanner is valid. */
+        MODEL_ASSERT(property_fido_scanner_valid(scanner));
+        /* the scanner is not at end of input. */
+        MODEL_ASSERT(0 != *(scanner->input));
+MODEL_CONTRACT_PRECONDITIONS_END(fido_scanner_next_character)
+
 /**
  * \brief Attempt to complete a role keyword token.
  *
