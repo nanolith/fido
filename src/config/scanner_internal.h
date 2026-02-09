536ea730e9ab8aa40298b78863c2c472918c8335
Justin Handville <nanolith@gmail.com>
2026-02-09 03:48:22 -0500

    fido_scanner_complete_token_keyword_env decl.

--- src/config/scanner_internal.h
+++ src/config/scanner_internal.h
@@ -334,6 +334,23 @@ MODEL_CONTRACT_POSTCONDITIONS_BEGIN(
         }
 MODEL_CONTRACT_POSTCONDITIONS_END(fido_scanner_complete_token_keyword_as)
 
+/**
+ * \brief Attempt to complete an env keyword token.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - FIDO_SCANNER_TOKEN_TYPE_KEYWORD_ENV or
+ *        FIDO_SCANNER_TOKEN_TYPE_USERNAME on success.
+ *      - FIDO_SCANNER_TOKEN_TYPE_BAD_INPUT if the scanner encounteres bad
+ *        input.
+ */
+int FN_DECL_MUST_CHECK
+fido_scanner_complete_token_keyword_env(
+    fido_token_details* details, fido_scanner* scanner);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
