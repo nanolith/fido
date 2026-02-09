63ab75d9a150ffb6183375638e1abf4f8336c91c
Justin Handville <nanolith@gmail.com>
2026-02-09 02:55:31 -0500

    fido_scanner_complete_token_keyword_cmd impl.

---
+++ src/config/fido_scanner_complete_token_keyword_cmd.c
@@ -0,0 +1,71 @@
+/**
+ * \file config/fido_scanner_complete_token_keyword_cmd.c
+ *
+ * \brief Complete the scan of the cmd keyword.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <ctype.h>
+
+#include "scanner_internal.h"
+
+/**
+ * \brief Attempt to complete a cmd keyword token.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - FIDO_SCANNER_TOKEN_TYPE_KEYWORD_CMD or
+ *        FIDO_SCANNER_TOKEN_TYPE_USERNAME on success.
+ *      - FIDO_SCANNER_TOKEN_TYPE_BAD_INPUT if the scanner encounteres bad
+ *        input.
+ */
+int FN_DECL_MUST_CHECK
+fido_scanner_complete_token_keyword_cmd(
+    fido_token_details* details, fido_scanner* scanner)
+{
+    int retval;
+
+    MODEL_CONTRACT_CHECK_PRECONDITIONS(
+        fido_scanner_complete_token_keyword_cmd, details, scanner);
+
+    if ('m' != *(scanner->input + 1))
+    {
+        goto username_fallback;
+    }
+    fido_scanner_next_character(scanner);
+
+    if ('d' != *(scanner->input + 1))
+    {
+        goto username_fallback;
+    }
+    fido_scanner_next_character(scanner);
+
+    if (isalnum(*(scanner->input + 1)))
+    {
+        goto username_fallback;
+    }
+    else
+    {
+        retval =
+            fido_scanner_token_details_end(
+                details, scanner, FIDO_SCANNER_TOKEN_TYPE_KEYWORD_CMD);
+
+        fido_scanner_next_character(scanner);
+
+        goto done;
+    }
+
+username_fallback:
+    retval = fido_scanner_complete_token_username(details, scanner);
+
+done:
+    MODEL_CONTRACT_CHECK_POSTCONDITIONS(
+        fido_scanner_complete_token_keyword_cmd, retval, details, scanner);
+
+    return retval;
+}
