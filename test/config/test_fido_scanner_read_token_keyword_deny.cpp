f1f040f15b4e59e75aab8187d4ed760e7084d76c
Justin Handville <nanolith@gmail.com>
2026-02-09 18:15:24 -0500

    Happy path unit test for deny keyword scanner.

---
+++ test/config/test_fido_scanner_read_token_keyword_deny.cpp
@@ -0,0 +1,43 @@
+/**
+ * \file test/config/test_fido_scanner_read_token_keyword_deny.c
+ *
+ * \brief Test that we can read the "deny" keyword.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <fido/scanner.h>
+#include <minunit/minunit.h>
+
+TEST_SUITE(fido_scanner_read_token_keyword_deny);
+
+/**
+ * \brief Basic test of the happy path.
+ */
+TEST(basics)
+{
+    fido_scanner* scanner = nullptr;
+    fido_token_details details;
+    const char* TEST_INPUT = "deny";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == fido_scanner_create(&scanner, TEST_INPUT));
+
+    /* read the deny keyword. */
+    TEST_ASSERT(
+        FIDO_SCANNER_TOKEN_TYPE_KEYWORD_DENY ==
+            fido_scanner_read_token_keyword_deny(&details, scanner));
+
+    /* the details are correct. */
+    TEST_EXPECT(FIDO_SCANNER_TOKEN_TYPE_KEYWORD_DENY == details.type);
+    TEST_EXPECT(0 == details.begin_index);
+    TEST_EXPECT(3 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(1 == details.begin_col);
+    TEST_EXPECT(4 == details.end_col);
+
+    /* clean up. */
+    fido_scanner_release(scanner);
+}
