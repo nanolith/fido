c3eeb354f9d54d5b2bc6d0793c72c26f11d1df24
Justin Handville <nanolith@gmail.com>
2026-02-09 01:54:29 -0500

    Don't begin the token in a completion routine.

--- src/config/fido_scanner_complete_token_username.c
+++ src/config/fido_scanner_complete_token_username.c
@@ -34,11 +34,6 @@ fido_scanner_complete_token_username(
     MODEL_CONTRACT_CHECK_PRECONDITIONS(
         fido_scanner_complete_token_username, details, scanner);
 
-    /* cache the start of the keyword. */
-    details->begin_index = scanner->index;
-    details->begin_col = scanner->col;
-    details->begin_line = scanner->line;
-
     peek = *(scanner->input + 1);
 
     while (isalnum(peek) || '_' == peek)
