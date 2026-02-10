47cbb7799686717547328fc32ce0bae45bf269aa
Justin Handville <nanolith@gmail.com>
2026-02-09 19:30:06 -0500

    Fixed typo in function documentation.

--- src/config/scanner_internal.h
+++ src/config/scanner_internal.h
@@ -261,7 +261,7 @@ MODEL_CONTRACT_POSTCONDITIONS_END(fido_scanner_complete_token_username)
  * \returns a token from the scanner.
  *      - FIDO_SCANNER_TOKEN_TYPE_KEYWORD_CMD or
  *        FIDO_SCANNER_TOKEN_TYPE_USERNAME on success.
- *      - FIDO_SCANNER_TOKEN_TYPE_BAD_INPUT if the scanner encounteres bad
+ *      - FIDO_SCANNER_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad
  *        input.
  */
 int FN_DECL_MUST_CHECK
