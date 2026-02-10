e506d5c9a0fe2ccb2d0c72de88326d83c03c22e3
Justin Handville <nanolith@gmail.com>
2026-02-10 01:48:51 -0500

    fido_scanner_read_token_add_variable impl.

---
+++ src/config/fido_scanner_read_token_add_variable.c
@@ -0,0 +1,62 @@
+/**
+ * \file config/fido_scanner_read_token_add_variable.c
+ *
+ * \brief Attempt to scan an add variable token.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <ctype.h>
+
+#include "scanner_internal.h"
+
+/**
+ * \brief Attempt to read an add variable token.
+ *
+ * \note This function exists mainly for verification purposes. The function,
+ * \ref fido_scanner_complete_token_identifier is used by the scanner
+ * proper, and called by this function.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - FIDO_SCANNER_TOKEN_TYPE_ADD_VARIABLE on success.
+ *      - FIDO_SCANNER_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad
+ *        input.
+ */
+int FN_DECL_MUST_CHECK
+fido_scanner_read_token_add_variable(
+    fido_token_details* details, fido_scanner* scanner)
+{
+    int retval;
+
+    MODEL_CONTRACT_CHECK_PRECONDITIONS(
+        fido_scanner_read_token_add_variable, details, scanner);
+
+    /* for this function, the token must begin with '+' */
+    if ('+' != *(scanner->input))
+    {
+        goto bad_input;
+    }
+
+    /* start the token. */
+    fido_scanner_token_details_begin(details, scanner);
+
+    /* attempt to complete this token. */
+    retval =
+        fido_scanner_complete_token_identifier(
+            details, scanner, FIDO_SCANNER_TOKEN_TYPE_ADD_VARIABLE, true);
+    goto done;
+
+bad_input:
+    retval = FIDO_SCANNER_TOKEN_TYPE_BAD_INPUT;
+
+done:
+    MODEL_CONTRACT_CHECK_POSTCONDITIONS(
+        fido_scanner_read_token_add_variable, retval, details, scanner);
+
+    return retval;
+}
