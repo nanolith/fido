a5f52d015cd0493ceee07beadb8bef551f59dcb1
Justin Handville <nanolith@gmail.com>
2026-02-09 19:28:58 -0500

    Fixed typo in function documentation.

--- include/fido/scanner.h
+++ include/fido/scanner.h
@@ -455,7 +455,7 @@ MODEL_CONTRACT_POSTCONDITIONS_END(fido_scanner_read_token_keyword_as)
  * \returns a token from the scanner.
  *      - FIDO_SCANNER_TOKEN_TYPE_KEYWORD_ENV or
  *        FIDO_SCANNER_TOKEN_TYPE_USERNAME on success.
- *      - FIDO_SCANNER_TOKEN_TYPE_BAD_INPUT if the scanner encounteres bad
+ *      - FIDO_SCANNER_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad
  *        input.
  */
 int FN_DECL_MUST_CHECK
