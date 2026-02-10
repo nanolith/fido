352fba6f2c508582ddb7a3cd0ef54e29bc2a2723
Justin Handville <nanolith@gmail.com>
2026-02-09 20:11:39 -0500

    Use identifier instead of username.

--- src/config/fido_scanner_complete_token_keyword_as.c
+++ src/config/fido_scanner_complete_token_keyword_as.c
@@ -55,7 +55,9 @@ fido_scanner_complete_token_keyword_as(
     }
 
 username_fallback:
-    retval = fido_scanner_complete_token_username(details, scanner);
+    retval =
+        fido_scanner_complete_token_identifier(
+            details, scanner, FIDO_SCANNER_TOKEN_TYPE_USERNAME);
 
 done:
     MODEL_CONTRACT_CHECK_POSTCONDITIONS(
