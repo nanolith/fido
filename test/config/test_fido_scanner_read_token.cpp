ea9aa695ad3fcf577d2c89c8925ea136842e9d7f
Justin Handville <nanolith@gmail.com>
2026-02-10 19:39:52 -0500

    Test that we can scan a permit keyword.

--- test/config/test_fido_scanner_read_token.cpp
+++ test/config/test_fido_scanner_read_token.cpp
@@ -242,3 +242,38 @@ TEST(env_keyword)
     /* clean up. */
     fido_scanner_release(scanner);
 }
+
+/**
+ * \brief We can read a permit keyword token.
+ */
+TEST(permit_keyword)
+{
+    fido_scanner* scanner = nullptr;
+    fido_token_details details;
+    const char* TEST_INPUT = " permit ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == fido_scanner_create(&scanner, TEST_INPUT));
+
+    /* read a token. */
+    TEST_ASSERT(
+        FIDO_SCANNER_TOKEN_TYPE_KEYWORD_PERMIT ==
+            fido_scanner_read_token(&details, scanner));
+
+    /* the details are correct. */
+    TEST_EXPECT(FIDO_SCANNER_TOKEN_TYPE_KEYWORD_PERMIT == details.type);
+    TEST_EXPECT(1 == details.begin_index);
+    TEST_EXPECT(6 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(2 == details.begin_col);
+    TEST_EXPECT(7 == details.end_col);
+
+    /* EOF ends input. */
+    TEST_ASSERT(
+        FIDO_SCANNER_TOKEN_TYPE_EOF ==
+            fido_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    fido_scanner_release(scanner);
+}
