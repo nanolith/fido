8ef3d8dc6e4f86a780e5a1deed27e6783d7ac3f8
Justin Handville <nanolith@gmail.com>
2026-02-10 19:36:44 -0500

    Test that we can read an as keyword token.

--- test/config/test_fido_scanner_read_token.cpp
+++ test/config/test_fido_scanner_read_token.cpp
@@ -172,3 +172,38 @@ TEST(cmd_keyword)
     /* clean up. */
     fido_scanner_release(scanner);
 }
+
+/**
+ * \brief We can read an as keyword token.
+ */
+TEST(as_keyword)
+{
+    fido_scanner* scanner = nullptr;
+    fido_token_details details;
+    const char* TEST_INPUT = " as ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == fido_scanner_create(&scanner, TEST_INPUT));
+
+    /* read a token. */
+    TEST_ASSERT(
+        FIDO_SCANNER_TOKEN_TYPE_KEYWORD_AS ==
+            fido_scanner_read_token(&details, scanner));
+
+    /* the details are correct. */
+    TEST_EXPECT(FIDO_SCANNER_TOKEN_TYPE_KEYWORD_AS == details.type);
+    TEST_EXPECT(1 == details.begin_index);
+    TEST_EXPECT(2 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(2 == details.begin_col);
+    TEST_EXPECT(3 == details.end_col);
+
+    /* EOF ends input. */
+    TEST_ASSERT(
+        FIDO_SCANNER_TOKEN_TYPE_EOF ==
+            fido_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    fido_scanner_release(scanner);
+}
