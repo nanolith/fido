f931d3750a93f7c2b3ffa2b552246322832f3756
Justin Handville <nanolith@gmail.com>
2026-02-11 00:25:52 -0500

    fido_scanner_complete_token_string decl.

--- src/config/scanner_internal.h
+++ src/config/scanner_internal.h
@@ -560,6 +560,22 @@ MODEL_CONTRACT_POSTCONDITIONS_BEGIN(
         }
 MODEL_CONTRACT_POSTCONDITIONS_END(fido_scanner_complete_token_keyword_deny)
 
+/**
+ * \brief Attempt to complete a string token.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - FIDO_SCANNER_TOKEN_TYPE_STRING on success.
+ *      - FIDO_SCANNER_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad
+ *        input.
+ */
+int FN_DECL_MUST_CHECK
+fido_scanner_complete_token_string(
+    fido_token_details* details, fido_scanner* scanner);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
