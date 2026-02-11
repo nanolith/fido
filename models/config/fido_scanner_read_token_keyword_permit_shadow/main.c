3b2b82b2e0b1b87de5a247eb9c8e0978eb412775
Justin Handville <nanolith@gmail.com>
2026-02-11 05:32:35 -0500

    fido_scanner_read_token_keyword_permit_shadow model checks.

---
+++ models/config/fido_scanner_read_token_keyword_permit_shadow/main.c
@@ -0,0 +1,39 @@
+/**
+ * \file models/config/fido_scanner_read_token_keyword_permit_shadow/main.c
+ *
+ * \brief Model checks for fido_scanner_read_token_keyword_permit_shadow.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <ctype.h>
+
+#include "config/scanner_internal.h"
+
+int main(int argc, char* argv[])
+{
+    char input[10];
+    int retval;
+    fido_scanner* scanner = NULL;
+    fido_token_details details;
+
+    /* randomize input, and ensure it is ASCIIZ. */
+    __CPROVER_havoc_object(input);
+    input[sizeof(input)-1] = 0;
+
+    /* create the scanner instance. */
+    retval = fido_scanner_create(&scanner, input);
+    if (0 != retval)
+    {
+        return 1;
+    }
+
+    /* attempt to read the permit keyword. */
+    retval = fido_scanner_read_token_keyword_permit(&details, scanner);
+
+    /* release the scanner instance. */
+    fido_scanner_release(scanner);
+
+    return 0;
+}
