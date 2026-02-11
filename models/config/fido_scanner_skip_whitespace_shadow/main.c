c70d7f99f71a22912b191ce19f72a0dfd0e5f331
Justin Handville <nanolith@gmail.com>
2026-02-11 05:56:53 -0500

    fido_scanner_skip_whitespace_shadow model checks.

---
+++ models/config/fido_scanner_skip_whitespace_shadow/main.c
@@ -0,0 +1,39 @@
+/**
+ * \file models/config/fido_scanner_skip_whitespace_shadow/main.c
+ *
+ * \brief Model checks for fido_scanner_skip_whitespace_shadow.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <ctype.h>
+
+#include "config/scanner_internal.h"
+
+int main(int argc, char* argv[])
+{
+    char input[10];
+    int retval;
+    fido_scanner* scanner = NULL;
+
+    /* randomize input, and ensure it is ASCIIZ. */
+    __CPROVER_havoc_object(input);
+    input[sizeof(input)-1] = 0;
+
+    /* create the scanner instance. */
+    retval = fido_scanner_create(&scanner, input);
+    if (0 != retval)
+    {
+        return 1;
+    }
+
+    /* get the next character that is not whitespace. */
+    retval = fido_scanner_skip_whitespace(scanner);
+    MODEL_ASSERT(!isspace(retval));
+
+    /* release the scanner instance. */
+    fido_scanner_release(scanner);
+
+    return 0;
+}
