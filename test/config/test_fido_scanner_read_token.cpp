8883289a09c3e5031c01078047e16e6ea11b6839
Justin Handville <nanolith@gmail.com>
2026-02-10 19:30:18 -0500

    Test that we can read an open brace token.

--- test/config/test_fido_scanner_read_token.cpp
+++ test/config/test_fido_scanner_read_token.cpp
@@ -32,3 +32,33 @@ TEST(empty_string_eof)
     /* clean up. */
     fido_scanner_release(scanner);
 }
+
+/**
+ * \brief We can read an open brace token.
+ */
+TEST(open_brace)
+{
+    fido_scanner* scanner = nullptr;
+    fido_token_details details;
+    const char* TEST_INPUT = " { ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == fido_scanner_create(&scanner, TEST_INPUT));
+
+    /* read a token. */
+    TEST_ASSERT(
+        FIDO_SCANNER_TOKEN_TYPE_OPEN_BRACE ==
+            fido_scanner_read_token(&details, scanner));
+
+    /* the details are correct. */
+    TEST_EXPECT(FIDO_SCANNER_TOKEN_TYPE_OPEN_BRACE == details.type);
+    TEST_EXPECT(1 == details.begin_index);
+    TEST_EXPECT(1 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(2 == details.begin_col);
+    TEST_EXPECT(2 == details.end_col);
+
+    /* clean up. */
+    fido_scanner_release(scanner);
+}
