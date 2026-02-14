9c2196872dd6861e5380110b75168abcaedcdad7
Justin Handville <nanolith@gmail.com>
2026-02-14 17:29:05 -0500

    fido_config_permission_release decl.

---
+++ src/config/parser_internal.h
@@ -0,0 +1,34 @@
+/**
+ * \file config/parser_internal.h
+ *
+ * \brief Internal data structures and methods relating to the fido config
+ * parser.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <fido/config.h>
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+/******************************************************************************/
+/* Start of constructors.                                                     */
+/******************************************************************************/
+
+/**
+ * \brief Release a \ref fido_config_permission instance.
+ *
+ * \param perm          The permission to release.
+ */
+void fido_config_permission_release(fido_config_permission* perm);
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
