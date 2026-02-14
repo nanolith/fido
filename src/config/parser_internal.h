2828b2e4db4c29b82961cc8b69eeb3c25b739e9e
Justin Handville <nanolith@gmail.com>
2026-02-14 17:31:28 -0500

    fido_config_permission_release function contract postconditions.

--- src/config/parser_internal.h
+++ src/config/parser_internal.h
@@ -35,6 +35,11 @@ MODEL_CONTRACT_PRECONDITIONS_BEGIN(
         MODEL_CHECK_OBJECT_RW(perm, sizeof(*perm));
 MODEL_CONTRACT_PRECONDITIONS_END(fido_config_permission_release)
 
+/* function contract postconditions. */
+MODEL_CONTRACT_POSTCONDITIONS_BEGIN(
+    fido_config_permission_release, fido_config_permission* perm)
+MODEL_CONTRACT_POSTCONDITIONS_END(fido_config_permission_release)
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
