d6829bc841ca9ec3101f92df87fdce513a838e36
Justin Handville <nanolith@gmail.com>
2026-02-21 00:52:49 -0500

    The permission parser returns an error when parsing an empty string.

---
+++ test/config/test_fido_config_parse_permission.cpp
@@ -0,0 +1,37 @@
+/**
+ * \file test/config/test_fido_config_parse_permission.cpp
+ *
+ * \brief Test that we can parse permission statements from the config.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <fido/scanner.h>
+#include <minunit/minunit.h>
+
+#include "../src/config/parser_internal.h"
+
+TEST_SUITE(fido_config_parse_permission);
+
+/**
+ * \brief Reading from an empty string results in an error.
+ */
+TEST(empty_string_error)
+{
+    fido_scanner* scanner = nullptr;
+    fido_config_permission* perm = nullptr;
+    const char* TEST_INPUT = "";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == fido_scanner_create(&scanner, TEST_INPUT));
+
+    /* attempt to parse a permission. */
+    TEST_ASSERT(
+        FIDO_ERROR_UNEXPECTED_EOF
+            == fido_config_parse_permission(&perm, scanner));
+    TEST_ASSERT(nullptr == perm);
+
+    /* clean up. */
+    fido_scanner_release(scanner);
+}
