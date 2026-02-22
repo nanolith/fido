bbe9d55b396e954e30ee7b37c9cb73d62f246cb2
Justin Handville <nanolith@gmail.com>
2026-02-22 01:32:02 -0500

    FIDO_ERROR_MULTIPLE_WILDCARD_ARGUMENTS decl.

--- include/fido/error_codes.h
+++ include/fido/error_codes.h
@@ -32,6 +32,8 @@ enum fido_error_code
     FIDO_ERROR_INVALID_STRING = 7,
     /** \brief An invalid escape sequence was encountered in a string. */
     FIDO_ERROR_INVALID_STRING_ESCAPE = 8,
+    /** \brief Multiple wildcard (*) arguments aren't supported. */
+    FIDO_ERROR_MULTIPLE_WILDCARD_ARGUMENTS = 9,
 };
 
 /* C++ compatibility. */
