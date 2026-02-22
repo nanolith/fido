aa14b7424c98f7a1fddc7fa52a6813257a2d71fe
Justin Handville <nanolith@gmail.com>
2026-02-21 23:36:51 -0500

    Test that the command parser returns an error when parsing an empty string.

---
+++ test/config/test_fido_config_parse_command.cpp
@@ -0,0 +1,38 @@
+/**
+ * \file test/config/test_fido_config_parse_command.cpp
+ *
+ * \brief Test that we can parse command statements from the config.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <fido/scanner.h>
+#include <minunit/minunit.h>
+#include <string.h>
+
+#include "../src/config/parser_internal.h"
+
+TEST_SUITE(fido_config_parse_command);
+
+/**
+ * \brief Reading from an empty string results in an error.
+ */
+TEST(empty_string_error)
+{
+    fido_scanner* scanner = nullptr;
+    fido_config_command* cmd = nullptr;
+    const char* TEST_INPUT = "";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == fido_scanner_create(&scanner, TEST_INPUT));
+
+    /* attempt to parse a permission. */
+    TEST_ASSERT(
+        FIDO_ERROR_UNEXPECTED_EOF
+            == fido_config_parse_command(&cmd, scanner));
+    TEST_ASSERT(nullptr == cmd);
+
+    /* clean up. */
+    fido_scanner_release(scanner);
+}
