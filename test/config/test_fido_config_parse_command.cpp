5e10bc84d16fca5178894e36ae271d3bfd9cfe50
Justin Handville <nanolith@gmail.com>
2026-02-22 12:02:40 -0500

    Parsing any token other than cmd results in an error.

--- test/config/test_fido_config_parse_command.cpp
+++ test/config/test_fido_config_parse_command.cpp
@@ -60,6 +60,28 @@ TEST(incomplete_cmd)
 }
 
 /**
+ * \brief Any other token read will result in an error.
+ */
+TEST(bad_start_token)
+{
+    fido_scanner* scanner = nullptr;
+    fido_config_command* cmd = nullptr;
+    const char* TEST_INPUT = "{";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == fido_scanner_create(&scanner, TEST_INPUT));
+
+    /* attempt to parse a command. */
+    TEST_ASSERT(
+        FIDO_ERROR_UNEXPECTED_TOKEN
+            == fido_config_parse_command(&cmd, scanner));
+    TEST_ASSERT(nullptr == cmd);
+
+    /* clean up. */
+    fido_scanner_release(scanner);
+}
+
+/**
  * \brief A command with an empty string results in an error.
  */
 TEST(cmd_empty_string)
