6f4377fb6006cd7af3129b06edf779937f26fcd0
Justin Handville <nanolith@gmail.com>
2026-05-12 19:48:47 -0400

    fido_config_create / fido_config_release model checks.

---
+++ models/config/fido_config_create_release/main.c
@@ -0,0 +1,30 @@
+/**
+ * \file models/config/fido_config_create_release/main.c
+ *
+ * \brief Model checks for fido config create / release.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <fido/config.h>
+
+#include "config/parser_internal.h"
+
+int main(int argc, char* argv[])
+{
+    int retval;
+    fido_config* config = NULL;
+
+    /* create the config instance. */
+    retval = fido_config_create(&config);
+    if (0 != retval)
+    {
+        return 1;
+    }
+
+    /* release the config instance. */
+    fido_config_release(config);
+
+    return 0;
+}
