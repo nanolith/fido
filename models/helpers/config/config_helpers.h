658673d96c9d4a7e855e2934fb5172ff125ca4c1
Justin Handville <nanolith@gmail.com>
2026-05-13 18:02:25 -0400

    fido_config_create_random shadow decl.

---
+++ models/helpers/config/config_helpers.h
@@ -0,0 +1,15 @@
+/**
+ * \file models/helpers/config/config_helpers.h
+ *
+ * \brief Helpers for exercising config data structures.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include "config/parser_internal.h"
+
+int FN_DECL_MUST_CHECK
+fido_config_create_random(fido_config** conf);
