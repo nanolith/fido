b0b4d5c44919408cb0ca7220e06ff24f40581384
Justin Handville <nanolith@gmail.com>
2026-05-13 19:19:30 -0400

    Refactored model checks in terms of helper.

--- models/config/fido_config_role_create_release/main.c
+++ models/config/fido_config_role_create_release/main.c
@@ -9,43 +9,16 @@
 
 #include <fido/config.h>
 
+#include "../../helpers/config/config_helpers.h"
 #include "config/parser_internal.h"
-
-int nondet_int();
-
-/* return the input string or NULL. */
-static const char* optional_string(const char* input)
-{
-    if (0 == nondet_int())
-    {
-        return input;
-    }
-    else
-    {
-        return NULL;
-    }
-}
 
 int main(int argc, char* argv[])
 {
     int retval;
     fido_config_role* role = NULL;
-    char name[8];
-    char as_user[8];
-    char as_group[8];
-
-    /* randomize inputs, and ensure they are ASCIIZ. */
-    __CPROVER_havoc_object(name);
-    name[sizeof(name)-1] = 0;
-    __CPROVER_havoc_object(as_user);
-    as_user[sizeof(as_user)-1] = 0;
-    __CPROVER_havoc_object(as_group);
-    as_group[sizeof(as_group)-1] = 0;
 
     /* create the role instance. */
-    retval =
-        fido_config_role_create(
-            &role, name, optional_string(as_user), optional_string(as_group));
+    retval = fido_config_role_create_random(&role);
     if (0 != retval)
     {
         return 1;
