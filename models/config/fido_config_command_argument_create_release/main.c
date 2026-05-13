f59bf06b0973abb160c76b065b9f34570da0576c
Justin Handville <nanolith@gmail.com>
2026-05-13 19:07:03 -0400

    Refactored model check in terms of helper method.

--- models/config/fido_config_command_argument_create_release/main.c
+++ models/config/fido_config_command_argument_create_release/main.c
@@ -9,34 +9,16 @@
 
 #include <fido/config.h>
 
+#include "../../helpers/config/config_helpers.h"
 #include "config/parser_internal.h"
-
-int nondet_int();
-
-static int fido_config_command_argument_argument_type()
-{
-    enum fido_config_command_argument_type value =
-        (enum fido_config_command_argument_type)nondet_int();
-    MODEL_ASSUME(__CPROVER_enum_is_in_range(value));
-
-    return value;
-}
 
 int main(int argc, char* argv[])
 {
     int retval;
     fido_config_command_argument* arg = NULL;
-    char argument_match[8];
-    int argument_type = fido_config_command_argument_argument_type();
-
-    /* randomize argument match, and ensure it is ASCIIZ. */
-    __CPROVER_havoc_object(argument_match);
-    argument_match[sizeof(argument_match)-1] = 0;
 
     /* create the command argument instance. */
-    retval =
-        fido_config_command_argument_create(
-            &arg, argument_type, argument_match);
+    retval = fido_config_command_argument_create_random(&arg);
     if (0 != retval)
     {
         return 1;
