e5d9b188b105a03c35c275100ee64c06897f8721
Justin Handville <nanolith@gmail.com>
2026-05-14 14:17:49 -0400

    fido_config_role_permissions_finalize shadow impl.

---
+++ models/shadow/config/fido_config_role_permissions_finalize.c
@@ -0,0 +1,25 @@
+/**
+ * \file models/shadow/config/fido_config_role_permissionss_finalize.c
+ *
+ * \brief This shadow form of \ref fido_config_role_permissionss_finalize is a
+ * no-op.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <stdlib.h>
+
+#include "config/parser_internal.h"
+
+void
+fido_config_role_permissionss_finalize(fido_config_role* role)
+{
+    MODEL_CONTRACT_CHECK_PRECONDITIONS(
+        fido_config_role_permissionss_finalize, role);
+
+    role->permissions_finalized = true;
+
+    MODEL_CONTRACT_CHECK_POSTCONDITIONS(
+        fido_config_role_permissionss_finalize, role);
+}
