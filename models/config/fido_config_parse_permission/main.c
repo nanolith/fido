339be0c66b9a95ceb46f52284d3b9c1f11c583aa
Justin Handville <nanolith@gmail.com>
2026-05-15 15:41:22 -0400

    fido_config_parse_permission model checks.

---
+++ models/config/fido_config_parse_permission/main.c
@@ -0,0 +1,52 @@
+/**
+ * \file models/config/fido_config_parse_permission/main.c
+ *
+ * \brief Model checks for \ref fido_config_parse_permission.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <fido/config.h>
+
+#include "../../helpers/config/config_helpers.h"
+#include "config/parser_internal.h"
+
+int main(int argc, char* argv[])
+{
+    char input[10];
+    int retval;
+    fido_scanner* scanner = NULL;
+    fido_config_permission* perm = NULL;
+
+    /* randomize input, and ensure it is ASCIIZ. */
+    __CPROVER_havoc_object(input);
+    input[sizeof(input)-1] = 0;
+
+    /* create the scanner instance. */
+    retval = fido_scanner_create(&scanner, input);
+    if (0 != retval)
+    {
+        goto done;
+    }
+
+    /* parse a permission instance. */
+    retval = fido_config_parse_permission(&perm, scanner);
+    if (0 != retval)
+    {
+        goto cleanup_scanner;
+    }
+
+    /* clean up. */
+    retval = 0;
+    goto cleanup_perm;
+
+cleanup_perm:
+    fido_config_permission_release(perm);
+
+cleanup_scanner:
+    fido_scanner_release(scanner);
+
+done:
+    return retval;
+}
