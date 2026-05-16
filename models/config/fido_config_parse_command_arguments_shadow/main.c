6c310fe03d1f9ab3a3bf58a1bf459adc1bfbc7a6
Justin Handville <nanolith@gmail.com>
2026-05-16 00:04:55 -0400

    fido_config_parse_command_arguments_shadow model checks.

---
+++ models/config/fido_config_parse_command_arguments_shadow/main.c
@@ -0,0 +1,48 @@
+/**
+ * \file models/config/fido_config_parse_command_arguments_shadow/main.c
+ *
+ * \brief Model checks for \ref fido_config_parse_command_arguments_shadow.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <fido/config.h>
+
+#include "../../helpers/config/config_helpers.h"
+#include "config/parser_internal.h"
+
+int main(int argc, char* argv[])
+{
+    char input[10];
+    int retval;
+    fido_config_command* cmd = NULL;
+
+    /* randomize input, and ensure it is ASCIIZ. */
+    __CPROVER_havoc_object(input);
+    input[sizeof(input)-1] = 0;
+
+    /* create the command instance. */
+    retval = fido_config_command_create_random(&cmd);
+    if (0 != retval)
+    {
+        goto done;
+    }
+
+    /* parse arguments. */
+    retval = fido_config_parse_command_arguments(cmd, input);
+    if (0 != retval)
+    {
+        goto cleanup_cmd;
+    }
+
+    /* clean up. */
+    retval = 0;
+    goto cleanup_cmd;
+
+cleanup_cmd:
+    fido_config_command_release(cmd);
+
+done:
+    return retval;
+}
