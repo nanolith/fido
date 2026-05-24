0a2ffa2024c8becd787789f11e5032f80951d0ad
Justin Handville <nanolith@gmail.com>
2026-05-24 19:39:28 -0400

    fido_options_create function contract postconditions.

--- include/fido/options.h
+++ include/fido/options.h
@@ -74,6 +74,26 @@ MODEL_CONTRACT_PRECONDITIONS_BEGIN(
         MODEL_CHECK_OBJECT_READ(argv, argc * sizeof(const char*));
 MODEL_CONTRACT_PRECONDITIONS_END(fido_options_create)
 
+/* function contract postconditions. */
+MODEL_CONTRACT_POSTCONDITIONS_BEGIN(
+    fido_options_create, int retval, fido_options** opts, int argc,
+    const char** argv)
+        /* on success... */
+        if (0 == retval)
+        {
+            /* the options are valid. */
+            MODEL_ASSERT(property_fido_options_valid(*opts));
+        }
+        else
+        {
+            /* this is a defined error code. */
+            enum fido_error_code error = (enum fido_error_code)retval;
+            MODEL_ASSERT(__CPROVER_enum_is_in_range(error));
+            /* opts is set to NULL. */
+            MODEL_ASSERT(NULL == *opts);
+        }
+MODEL_CONTRACT_POSTCONDITIONS_END(fido_options_create)
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
