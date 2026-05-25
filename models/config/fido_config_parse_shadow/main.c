12f82d63d7ec79e7a39f970cbeb7f5fc49fd9d88
Justin Handville <nanolith@gmail.com>
2026-05-25 16:16:12 -0400

    Verify that instance variables are set to NULL on error paths.

--- models/config/fido_config_parse_shadow/main.c
+++ models/config/fido_config_parse_shadow/main.c
@@ -18,7 +18,7 @@ int main(int argc, char* argv[])
 {
     char input[3];
     int retval;
-    fido_config* config = NULL;
+    fido_config* config;
 
     /* randomize input, and ensure it is ASCIIZ. */
     __CPROVER_havoc_object(input);
