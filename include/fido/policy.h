1abccadebdd43e81e202440c0e5b671572bb46de
Justin Handville <nanolith@gmail.com>
2026-05-25 18:21:34 -0400

    Fixed preconditions.

--- include/fido/policy.h
+++ include/fido/policy.h
@@ -98,13 +98,14 @@ fido_policy_role_match(
 /* function contract preconditions. */
 MODEL_CONTRACT_PRECONDITIONS_BEGIN(
     fido_policy_role_match, const char** as_user, const char** as_group,
-    const fido_config* config, const fido_options* opts, const fido_user* user)
+    const fido_config_role* role, const fido_options* opts,
+    const fido_user* user)
         /* the as_user pointer is valid. */
         MODEL_CHECK_OBJECT_RW(as_user, sizeof(*as_user));
         /* the as_group pointer is valid. */
         MODEL_CHECK_OBJECT_RW(as_group, sizeof(*as_group));
-        /* config is valid. */
-        MODEL_ASSERT(property_fido_config_valid(config));
+        /* role is valid. */
+        MODEL_ASSERT(property_fido_config_role_valid(role));
         /* options is valid. */
         MODEL_ASSERT(property_fido_options_valid(options));
         /* user is valid. */
