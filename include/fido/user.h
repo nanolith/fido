45c0f8768cd644f0a71445f60c618ed17fed46d8
Justin Handville <nanolith@gmail.com>
2026-05-25 19:58:20 -0400

    fido_user_create function contract postconditions.

--- include/fido/user.h
+++ include/fido/user.h
@@ -68,6 +68,25 @@ MODEL_CONTRACT_PRECONDITIONS_BEGIN(
         MODEL_CHECK_OBJECT_RW(user, sizeof(*user));
 MODEL_CONTRACT_PRECONDITIONS_END(fido_user_create)
 
+/* function contract postconditions. */
+MODEL_CONTRACT_POSTCONDITIONS_BEGIN(
+    fido_user_create, int retval, fido_user** user)
+        /* on success... */
+        if (0 == retval)
+        {
+            /* the user is valid. */
+            MODEL_ASSERT(property_fido_user_valid(*user));
+        }
+        else
+        {
+            /* this is a defined error code. */
+            enum fido_error_code error = (enum fido_error_code)retval;
+            MODEL_ASSERT(__CPROVER_enum_is_in_range(error));
+            /* user is set to NULL. */
+            MODEL_ASSERT(NULL == *user);
+        }
+MODEL_CONTRACT_POSTCONDITIONS_END(fido_user_create)
+
 /**
  * \brief Release a \ref fido_user instance.
  *
