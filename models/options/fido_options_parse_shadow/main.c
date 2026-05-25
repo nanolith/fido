d3be08e1b2e53203d8adc64d95558ba4d841008a
Justin Handville <nanolith@gmail.com>
2026-05-25 14:35:59 -0400

    fido_options_parse_shadow model checks.

---
+++ models/options/fido_options_parse_shadow/main.c
@@ -0,0 +1,47 @@
+/**
+ * \file models/options/fido_options_parse_shadow/main.c
+ *
+ * \brief Model checks for the \ref fido_options_parse shadow method.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <fido/options.h>
+
+#include "../../helpers/options/options_helpers.h"
+
+static char argument0[8];
+static char argument1[8];
+static char argument2[8];
+
+static char* argvec[3] = {argument0, argument1, argument2};
+
+int main(int argc, char* argv[])
+{
+    int retval;
+    fido_options* opts = NULL;
+
+    __CPROVER_havoc_object(argument0);
+    argument0[sizeof(argument0)-1] = 0;
+    __CPROVER_havoc_object(argument1);
+    argument1[sizeof(argument1)-1] = 0;
+    __CPROVER_havoc_object(argument2);
+    argument2[sizeof(argument2)-1] = 0;
+
+    MODEL_ASSUME(argc >= 0 && argc <= 3);
+
+    /* parse options. */
+    retval = fido_options_parse(&opts, argc, argvec);
+    if (0 != retval)
+    {
+        goto done;
+    }
+
+    /* release the options instance. */
+    fido_options_release(opts);
+    retval = 0;
+
+done:
+    return retval;
+}
