92d8b51f13805dada9366658a559fa5f3fa55efe
Justin Handville <nanolith@gmail.com>
2026-05-25 17:34:24 -0400

    Updated function contract preconditions.

--- include/fido/policy.h
+++ include/fido/policy.h
@@ -41,7 +41,7 @@ fido_policy_check(
 /* function contract preconditions. */
 MODEL_CONTRACT_PRECONDITIONS_BEGIN(
     fido_policy_check, const char** as_user, const char** as_group,
-    const fido_config* config, const fido_options* opts)
+    const fido_config* config, const fido_options* opts, const fido_user* user)
         /* the as_user pointer is valid. */
         MODEL_CHECK_OBJECT_RW(as_user, sizeof(*as_user));
         /* the as_group pointer is valid. */
@@ -50,6 +50,8 @@ MODEL_CONTRACT_PRECONDITIONS_BEGIN(
         MODEL_ASSERT(property_fido_config_valid(config));
         /* options is valid. */
         MODEL_ASSERT(property_fido_options_valid(options));
+        /* user is valid. */
+        MODEL_ASSERT(property_fido_user_valid(user));
 MODEL_CONTRACT_PRECONDITIONS_END(fido_policy_check)
 
 /* function contract postconditions. */
