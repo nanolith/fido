138b7fdcba32ba6dcc8317d205d6e0fb86e73741
Justin Handville <nanolith@gmail.com>
2026-05-26 16:25:34 -0400

    fido_user_create model checks.

---
+++ models/user/fido_user_create/main.c
@@ -0,0 +1,32 @@
+/**
+ * \file models/user/fido_user_create/main.c
+ *
+ * \brief Model checks for \ref fido_user_create.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <fido/options.h>
+
+#include "../../helpers/user/user_helpers.h"
+
+int main(int argc, char* argv[])
+{
+    int retval;
+    fido_user* user;
+
+    /* create the user instance. */
+    retval = fido_user_create(&user);
+    if (0 != retval)
+    {
+        goto done;
+    }
+
+    /* release the user instance. */
+    fido_user_release(user);
+    retval = 0;
+
+done:
+    return retval;
+}
