ef82a63e1e8176d25f7070f0f833af0cf7a70b4c
Justin Handville <nanolith@gmail.com>
2026-05-26 22:19:34 -0400

    Test that we can match exact arguments.

---
+++ test/policy/test_fido_policy_command_argument_match.cpp
@@ -0,0 +1,43 @@
+/**
+ * \file test/policy/test_fido_policy_command_argument_match.cpp
+ *
+ * \brief Test that we can match command arguments with a policy matcher.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <fido/config.h>
+#include <fido/policy.h>
+#include <minunit/minunit.h>
+
+TEST_SUITE(fido_policy_command_argument_match);
+
+/**
+ * \brief Test that we can match an exact argument.
+ */
+TEST(exact_argument)
+{
+    fido_config* config = nullptr;
+    const char* TEST_INPUT = R"(
+        role "foo" {
+            cmd "/sbin/mount /dev/sd1c /mnt"
+        }
+    )";
+
+    TEST_ASSERT(0 == fido_config_parse(&config, TEST_INPUT));
+    TEST_ASSERT(nullptr != config);
+    TEST_ASSERT(nullptr != config->head);
+    TEST_ASSERT(nullptr != config->head->command_head);
+    TEST_ASSERT(nullptr != config->head->command_head->head);
+
+    fido_config_command_argument* arg = config->head->command_head->head;
+
+    /* test that an exact match passes. */
+    TEST_ASSERT(0 == fido_policy_command_argument_match(arg, "/dev/sd1c"));
+    /* test that anything else fails. */
+    TEST_ASSERT(0 != fido_policy_command_argument_match(arg, "/dev/sd2a"));
+
+    /* clean up. */
+    fido_config_release(config);
+}
