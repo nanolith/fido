84f84e7106a29731e0da2cc9c4de592b60d977e9
Justin Handville <nanolith@gmail.com>
2026-05-27 17:42:55 -0400

    Test that matching can fail on a permit.

--- test/policy/test_fido_policy_role_match.cpp
+++ test/policy/test_fido_policy_role_match.cpp
@@ -107,3 +107,45 @@ TEST(match_role_permit)
     fido_options_release(opts);
     fido_user_release(user);
 }
+
+/**
+ * \brief Test that users not explicitly authorized don't match.
+ */
+TEST(nomatch_role_permit)
+{
+    fido_config* config = nullptr;
+    fido_options* opts = nullptr;
+    fido_user* user = nullptr;
+    const char* as_user = nullptr;
+    const char* as_group = nullptr;
+    const fido_config_add_variable* env_head = nullptr;
+    const char* TEST_INPUT = R"(
+        role "foo" {
+            permit alice
+        }
+    )";
+    const char** ARGS = nullptr;
+    size_t ARGS_COUNT = 0;
+
+    TEST_ASSERT(0 == fido_user_create_test(&user, "bob", "wheel", "bob"));
+    TEST_ASSERT(
+        0
+            == fido_options_create_test(
+                    &opts, "/sbin/shutdown", ARGS_COUNT, ARGS));
+    TEST_ASSERT(0 == fido_config_parse(&config, TEST_INPUT));
+    TEST_ASSERT(nullptr != config);
+    TEST_ASSERT(nullptr != config->head);
+
+    fido_config_role* role = config->head;
+
+    /* test that we don't match this role. */
+    TEST_ASSERT(
+        0
+            != fido_policy_role_match(
+                    &as_user, &as_group, &env_head, role, opts, user));
+
+    /* clean up. */
+    fido_config_release(config);
+    fido_options_release(opts);
+    fido_user_release(user);
+}
