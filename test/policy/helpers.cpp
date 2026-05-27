2a6c2291c152a31c036c9f28fd5c361495a38088
Justin Handville <nanolith@gmail.com>
2026-05-27 15:38:11 -0400

    fido_user_create_test impl.

--- test/policy/helpers.cpp
+++ test/policy/helpers.cpp
@@ -40,3 +40,74 @@ int fido_options_create_test(
 done:
     return retval;
 }
+
+int fido_user_create_test(
+    fido_user** user, const char* username, const char* group1,
+    const char* group2)
+{
+    int retval;
+    fido_user* tmp;
+    size_t gid_size, groupnames_size;
+
+    tmp = (fido_user*)malloc(sizeof(*tmp));
+    if (NULL == tmp)
+    {
+        retval = FIDO_ERROR_OUT_OF_MEMORY;
+        goto done;
+    }
+
+    memset(tmp, 0, sizeof(*tmp));
+
+    tmp->username = strdup(username);
+    if (NULL == tmp->username)
+    {
+        retval = FIDO_ERROR_OUT_OF_MEMORY;
+        goto cleanup_tmp;
+    }
+
+    tmp->group_count = 2;
+
+    gid_size = tmp->group_count * sizeof(gid_t);
+    tmp->gids = (gid_t*)malloc(gid_size);
+    if (NULL == tmp->gids)
+    {
+        retval = FIDO_ERROR_OUT_OF_MEMORY;
+        goto cleanup_tmp;
+    }
+    tmp->gids[0] = 0;
+    tmp->gids[1] = 1;
+
+    groupnames_size = tmp->group_count * sizeof(char*);
+    tmp->groupnames = (char**)malloc(groupnames_size);
+    if (NULL == tmp->groupnames)
+    {
+        retval = FIDO_ERROR_OUT_OF_MEMORY;
+        goto cleanup_tmp;
+    }
+    tmp->groupnames[0] = NULL;
+    tmp->groupnames[1] = NULL;
+
+    tmp->groupnames[0] = strdup(group1);
+    if (NULL == tmp->groupnames[0])
+    {
+        retval = FIDO_ERROR_OUT_OF_MEMORY;
+        goto cleanup_tmp;
+    }
+
+    tmp->groupnames[1] = strdup(group2);
+    if (NULL == tmp->groupnames[1])
+    {
+        retval = FIDO_ERROR_OUT_OF_MEMORY;
+        goto cleanup_tmp;
+    }
+
+    retval = 0;
+    *user = tmp;
+    goto done;
+
+cleanup_tmp:
+    fido_user_release(tmp);
+
+done:
+    return retval;
+}
--- test/policy/test_fido_policy_permission_match.cpp
+++ test/policy/test_fido_policy_permission_match.cpp
@@ -13,78 +13,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "helpers.h"
+
 TEST_SUITE(fido_policy_permission_match);
-
-static int fido_user_create_test(
-    fido_user** user, const char* username, const char* group1,
-    const char* group2)
-{
-    int retval;
-    fido_user* tmp;
-    size_t gid_size, groupnames_size;
-
-    tmp = (fido_user*)malloc(sizeof(*tmp));
-    if (NULL == tmp)
-    {
-        retval = FIDO_ERROR_OUT_OF_MEMORY;
-        goto done;
-    }
-
-    memset(tmp, 0, sizeof(*tmp));
-
-    tmp->username = strdup(username);
-    if (NULL == tmp->username)
-    {
-        retval = FIDO_ERROR_OUT_OF_MEMORY;
-        goto cleanup_tmp;
-    }
-
-    tmp->group_count = 2;
-
-    gid_size = tmp->group_count * sizeof(gid_t);
-    tmp->gids = (gid_t*)malloc(gid_size);
-    if (NULL == tmp->gids)
-    {
-        retval = FIDO_ERROR_OUT_OF_MEMORY;
-        goto cleanup_tmp;
-    }
-    tmp->gids[0] = 0;
-    tmp->gids[1] = 1;
-
-    groupnames_size = tmp->group_count * sizeof(char*);
-    tmp->groupnames = (char**)malloc(groupnames_size);
-    if (NULL == tmp->groupnames)
-    {
-        retval = FIDO_ERROR_OUT_OF_MEMORY;
-        goto cleanup_tmp;
-    }
-    tmp->groupnames[0] = NULL;
-    tmp->groupnames[1] = NULL;
-
-    tmp->groupnames[0] = strdup(group1);
-    if (NULL == tmp->groupnames[0])
-    {
-        retval = FIDO_ERROR_OUT_OF_MEMORY;
-        goto cleanup_tmp;
-    }
-
-    tmp->groupnames[1] = strdup(group2);
-    if (NULL == tmp->groupnames[1])
-    {
-        retval = FIDO_ERROR_OUT_OF_MEMORY;
-        goto cleanup_tmp;
-    }
-
-    retval = 0;
-    *user = tmp;
-    goto done;
-
-cleanup_tmp:
-    fido_user_release(tmp);
-
-done:
-    return retval;
-}
 
 /**
  * \brief Test that we can match a permit user permission.
