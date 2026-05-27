6f4af01f12886b248805043d34618c31f4b32a87
Justin Handville <nanolith@gmail.com>
2026-05-27 17:44:33 -0400

    Test that we can override the user.

--- test/policy/test_fido_policy_role_match.cpp
+++ test/policy/test_fido_policy_role_match.cpp
@@ -275,3 +275,45 @@ TEST(nomatch_role_last_permission_wins)
     fido_options_release(opts);
     fido_user_release(user);
 }
+
+/**
+ * \brief Test that we can override the user.
+ */
+TEST(match_role_as_user_override)
+{
+    fido_config* config = nullptr;
+    fido_options* opts = nullptr;
+    fido_user* user = nullptr;
+    const char* as_user = nullptr;
+    const char* as_group = nullptr;
+    const fido_config_add_variable* env_head = nullptr;
+    const char* TEST_INPUT = R"(
+        role "foo" {
+            permit :wheel
+            as operator
+        }
+    )";
+    const char** ARGS = nullptr;
+    size_t ARGS_COUNT = 0;
+
+    TEST_ASSERT(0 == fido_user_create_test(&user, "bob", "wheel", "bob"));
+    TEST_ASSERT(
+        0
+            == fido_options_create_test(
+                    &opts, "/sbin/shutdown", ARGS_COUNT, ARGS));
+    TEST_ASSERT(0 == fido_config_parse(&config, TEST_INPUT));
+
+    fido_config_role* role = config->head;
+
+    /* test that we match this role. */
+    TEST_ASSERT(
+        0
+            == fido_policy_role_match(
+                    &as_user, &as_group, &env_head, role, opts, user));
+    TEST_ASSERT(!strcmp("operator", as_user));
+
+    /* clean up. */
+    fido_config_release(config);
+    fido_options_release(opts);
+    fido_user_release(user);
+}
