f8e708319280f3600c0ef19fcb9de18e22b4cb41
Justin Handville <nanolith@gmail.com>
2026-05-27 17:43:21 -0400

    Test that we can match on a group permit.

--- test/policy/test_fido_policy_role_match.cpp
+++ test/policy/test_fido_policy_role_match.cpp
@@ -149,3 +149,48 @@ TEST(nomatch_role_permit)
     fido_options_release(opts);
     fido_user_release(user);
 }
+
+/**
+ * \brief Test that users with group membership can be authorized.
+ */
+TEST(match_role_group_permit)
+{
+    fido_config* config = nullptr;
+    fido_options* opts = nullptr;
+    fido_user* user = nullptr;
+    const char* as_user = nullptr;
+    const char* as_group = nullptr;
+    const fido_config_add_variable* env_head = nullptr;
+    const char* TEST_INPUT = R"(
+        role "foo" {
+            permit :bob
+        }
+    )";
+    const char** ARGS = nullptr;
+    size_t ARGS_COUNT = 0;
+
+    TEST_ASSERT(0 == fido_user_create_test(&user, "bob", "wheel", "bob"));
+    TEST_ASSERT(
+        0
+            == fido_options_create_test(
+                    &opts, "/sbin/shutdown", ARGS_COUNT, ARGS));
+    TEST_ASSERT(0 == fido_config_parse(&config, TEST_INPUT));
+    TEST_ASSERT(nullptr != config);
+    TEST_ASSERT(nullptr != config->head);
+
+    fido_config_role* role = config->head;
+
+    /* test that we match this role. */
+    TEST_ASSERT(
+        0
+            == fido_policy_role_match(
+                    &as_user, &as_group, &env_head, role, opts, user));
+    TEST_ASSERT(!strcmp("root", as_user));
+    TEST_ASSERT(!strcmp("wheel", as_group));
+    TEST_ASSERT(nullptr == env_head);
+
+    /* clean up. */
+    fido_config_release(config);
+    fido_options_release(opts);
+    fido_user_release(user);
+}
