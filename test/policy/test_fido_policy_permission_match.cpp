20cecc7628775ee7abb2b0b4e3a3ca4cd84543f2
Justin Handville <nanolith@gmail.com>
2026-05-27 00:51:27 -0400

    Test that we can fail to match a deny group.

--- test/policy/test_fido_policy_permission_match.cpp
+++ test/policy/test_fido_policy_permission_match.cpp
@@ -346,3 +346,32 @@ TEST(nomatch_permit_group)
     fido_config_release(config);
     fido_user_release(user);
 }
+
+/**
+ * \brief Test that we fail to match a deny group permission.
+ */
+TEST(nomatch_deny_group)
+{
+    fido_user* user = nullptr;
+    fido_config* config = nullptr;
+    const char* TEST_INPUT = R"(
+        role "foo" {
+            deny :operator
+        }
+    )";
+
+    TEST_ASSERT(0 == fido_user_create_test(&user, "bob", "wheel", "bob"));
+    TEST_ASSERT(0 == fido_config_parse(&config, TEST_INPUT));
+    TEST_ASSERT(nullptr != config);
+    TEST_ASSERT(nullptr != config->head);
+    TEST_ASSERT(nullptr != config->head->permission_head);
+
+    fido_config_permission* perm = config->head->permission_head;
+
+    /* test that we don't match. */
+    TEST_ASSERT(0 != fido_policy_permission_match(perm, user));
+
+    /* clean up. */
+    fido_config_release(config);
+    fido_user_release(user);
+}
