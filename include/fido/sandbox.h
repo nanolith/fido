2053c554511a6dc6cce7588a61083dfdc863f9fb
Justin Handville <nanolith@gmail.com>
2026-05-29 00:30:15 -0400

    sandbox_enter decl.

---
+++ include/fido/sandbox.h
@@ -0,0 +1,32 @@
+/**
+ * \file fido/sandbox.h
+ *
+ * \brief Sandbox related methods.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <fido/function_contracts.h>
+#include <fido/function_decl.h>
+#include <fido/model_assert.h>
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+/**
+ * \brief Enter sandbox mode via capsicum on FreeBSD or pledge / unveil on
+ * OpenBSD.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int FN_DECL_MUST_CHECK sandbox_enter(void);
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
