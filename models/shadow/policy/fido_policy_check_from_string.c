82b6d7d3e56158fe44f67df4cec0c468b5b651f6
Justin Handville <nanolith@gmail.com>
2026-05-29 11:43:46 -0400

    fido_policy_check_from_string shadow impl.

---
+++ models/shadow/policy/fido_policy_check_from_string.c
@@ -0,0 +1,30 @@
+/**
+ * \file models/shadow/policy/fido_policy_check_from_string.c
+ *
+ * \brief Shadow implementation of \ref fido_policy_check_from_string.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <fido/policy.h>
+
+int nondet_retval();
+
+int FN_DECL_MUST_CHECK
+fido_policy_check_from_string(
+    const fido_user* user, const fido_options* opts, const char* config_str,
+    bool authoritative)
+{
+    MODEL_CONTRACT_CHECK_PRECONDITIONS(
+        fido_policy_check_from_string, user, opts, config_str, authoritative);
+
+    int retval = nondet_retval();
+    MODEL_ASSUME(0 == retval || FIDO_ERROR_POLICY_MATCH == retval);
+
+    MODEL_CONTRACT_CHECK_POSTCONDITIONS(
+        fido_policy_check_from_string, retval, user, opts, config_str,
+        authoritative);
+
+    return retval;
+}
