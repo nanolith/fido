ea81438ccb64831312917ed17dce335dbfed204b
Justin Handville <nanolith@gmail.com>
2026-05-29 20:05:43 -0400

    open shadow impl.

---
+++ models/shadow/unix/open.c
@@ -0,0 +1,78 @@
+/**
+ * \file models/shadow/unix/open.c
+ *
+ * \brief open shadow implementation.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <fido/model_assert.h>
+#include <fido/properties/unix.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <sys/stat.h>
+
+#include "unix_shadow.h"
+
+int nondet_retval();
+
+int open(const char *path, int flags, ...)
+{
+    MODEL_ASSERT(NULL != path);
+    MODEL_ASSERT(O_RDONLY == flags);
+
+    int retval = nondet_retval();
+    MODEL_ASSUME(
+        -1 == retval || (retval >= 0 && retval < MAX_SHADOW_DESCRIPTORS));
+
+    if (retval >= 0)
+    {
+        if (NULL == shadow_descriptors[retval])
+        {
+            shadow_descriptors[retval] =
+                (shadow_file_descriptor*)malloc(sizeof(shadow_file_descriptor));
+            if (NULL == shadow_descriptors[retval])
+            {
+                retval = -1;
+            }
+            else
+            {
+                shadow_descriptors[retval]->mode = SHADOW_FILE_DESCRIPTOR_OPEN;
+            }
+        }
+    }
+
+    if (retval < 0)
+    {
+        errno = nondet_retval();
+        MODEL_ASSUME(EPERM == errno || ENOENT == errno);
+    }
+
+    return retval;
+}
+
+int fstat(int fd, struct stat *sb)
+{
+    MODEL_ASSERT(property_file_descriptor_open(fd));
+    MODEL_CHECK_OBJECT_RW(sb, sizeof(struct stat));
+
+    int retval = nondet_retval();
+    MODEL_ASSUME(0 == retval || -1 == retval);
+
+    if (0 == retval)
+    {
+        __CPROVER_havoc_object(sb);
+        sb->st_dev = nondet_dev();
+        sb->st_ino = nondet_inode();
+        sb->st_nlink = nondet_nlink();
+        sb->st_mode = nondet_mode();
+        sb->st_bsdflags = nondet_int16();
+        sb->st_uid = nondet_uid();
+        sb->st_gid = nondet_gid();
+    }
+
+    return retval;
+}
