2f90737d1f42003cc89cf6691af416c74ac80a05
Justin Handville <nanolith@gmail.com>
2026-05-31 14:06:23 -0400

    Refactored method in terms of parse from string.

--- src/policy_proc/fido_policy_decision_parse_from_descriptor.c
+++ src/policy_proc/fido_policy_decision_parse_from_descriptor.c
@@ -16,14 +16,8 @@
 
 #define MAX_BUFFER_SIZE (64 * 1024)
 
-static const char *USER_GROUP_CHARSET =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-";
-static const char *VARIABLE_CHARSET =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_";
-
 /* forward decls. */
 static int read_policy_string(char* buffer, size_t buffer_size, int fd);
-static int validate_string(const char* str, const char* allowed_set);
 
 /**
  * \brief Parse a \ref fido_policy_decision instance from a descriptor.
@@ -40,120 +34,28 @@ int FN_DECL_MUST_CHECK
 fido_policy_decision_parse_from_descriptor(fido_policy_decision** dec, int fd)
 {
     int retval;
-    fido_policy_decision* tmp;
     char buffer[MAX_BUFFER_SIZE];
-    char* str = buffer;
 
     MODEL_CONTRACT_CHECK_PRECONDITIONS(
         fido_policy_decision_parse_from_descriptor, dec, fd);
-
-    /* allocate memory for this policy decision. */
-    tmp = (fido_policy_decision*)malloc(sizeof(*tmp));
-    if (NULL == tmp)
-    {
-        retval = FIDO_ERROR_OUT_OF_MEMORY;
-        goto done;
-    }
-    memset(tmp, 0, sizeof(*tmp));
 
     /* read the decision. */
     retval = read_policy_string(buffer, sizeof(buffer), fd);
     if (0 != retval)
     {
-        goto cleanup_tmp;
-    }
-
-    /* decode the action. */
-    const char* action = strsep(&str, ":");
-
-    /* short circuit a denial. */
-    if (!strcmp(action, "deny"))
-    {
-        tmp->policy_decision = FIDO_POLICY_DECISION_DENY;
-        *dec = tmp;
-        retval = 0;
         goto done;
     }
 
-    /* if this is NOT a permit, then we've hit a parse error. */
-    if (strcmp(action, "permit"))
+    /* parse this string. */
+    retval = fido_policy_decision_parse_from_string(dec, buffer);
+    if (0 != retval)
     {
-        fprintf(stderr, "error: fido policy read.\n");
-        retval = FIDO_ERROR_POLICY_READ;
-        goto cleanup_tmp;
+        goto done;
     }
 
-    /* proceed with permit. */
-    tmp->policy_decision = FIDO_POLICY_DECISION_PERMIT;
-
-    /* parse the as user. */
-    const char* as_user = strsep(&str, ":");
-    retval = validate_string(as_user, USER_GROUP_CHARSET);
-    if (0 != retval || '-' == as_user[0])
-    {
-        fprintf(stderr, "error: invalid user.\n");
-        retval = FIDO_ERROR_INVALID_INPUT;
-        goto cleanup_tmp;
-    }
-
-    /* save this value. */
-    tmp->as_user = strdup(as_user);
-    if (NULL == tmp->as_user)
-    {
-        retval = FIDO_ERROR_OUT_OF_MEMORY;
-        goto cleanup_tmp;
-    }
-
-    /* parse the group. */
-    const char* as_group = strsep(&str, ":");
-    retval = validate_string(as_group, USER_GROUP_CHARSET);
-    if (0 != retval || '-' == as_group[0])
-    {
-        fprintf(stderr, "error: invalid group.\n");
-        retval = FIDO_ERROR_INVALID_INPUT;
-        goto cleanup_tmp;
-    }
-
-    /* save this value. */
-    tmp->as_group = strdup(as_group);
-    if (NULL == tmp->as_group)
-    {
-        retval = FIDO_ERROR_OUT_OF_MEMORY;
-        goto cleanup_tmp;
-    }
-
-    /* parse any add variables. */
-    char* add_variable_list = strsep(&str, ":");
-    while (NULL != add_variable_list)
-    {
-        const char* var = strsep(&add_variable_list, ",");
-        retval = validate_string(var, VARIABLE_CHARSET);
-        if (0 != retval)
-        {
-            fprintf(stderr, "error: invalid variable.\n");
-            retval = FIDO_ERROR_INVALID_INPUT;
-            goto cleanup_tmp;
-        }
-
-        fido_config_add_variable* vtmp;
-        retval = fido_config_add_variable_create(&vtmp, var);
-        if (0 != retval)
-        {
-            retval = FIDO_ERROR_OUT_OF_MEMORY;
-            goto cleanup_tmp;
-        }
-
-        vtmp->next = tmp->variable_head;
-        tmp->variable_head = vtmp;
-    }
-
-    /* success. */
-    *dec = tmp;
+    /* goto done. */
     retval = 0;
     goto done;
-
-cleanup_tmp:
-    fido_policy_decision_release(tmp);
 
 done:
     if (0 != retval)
@@ -165,33 +67,6 @@ done:
         fido_policy_decision_parse_from_descriptor, retval, dec, fd);
 
     return retval;
-}
-
-/**
- * \brief Validate a parsed string.
- *
- * \param str           The string to validate.
- * \param allowed_set   The allowed set of characters.
- *
- * \returns a status code indicating success or failure.
- *      - 0 on success.
- *      - a non-zero error code on failure.
- */
-static int validate_string(const char* str, const char* allowed_set)
-{
-    /* return error if this string couldn't be parsed or is empty. */
-    if (NULL == str || 0 == str[0])
-    {
-        return FIDO_ERROR_INVALID_INPUT;
-    }
-
-    /* if a non-matching character is found, this is an error. */
-    if (strlen(str) != strspn(str, allowed_set))
-    {
-        return FIDO_ERROR_INVALID_INPUT;
-    }
-
-    return 0;
 }
 
 /**
