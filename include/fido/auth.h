0b81764c78cd619d4833113590a4fcde8b559f98
Justin Handville <nanolith@gmail.com>
2026-06-01 15:41:21 -0400

    fido_auth_challenge decl.

---
+++ include/fido/auth.h
@@ -0,0 +1,38 @@
+/**
+ * \file fido/policy_proc.h
+ *
+ * \brief Portable methods for interfacing with authentication frameworks.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <fido/user.h>
+#include <fido/function_contracts.h>
+#include <fido/function_decl.h>
+#include <fido/model_assert.h>
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+/**
+ * \brief Provide an authentication challenge, read the response, and verify it
+ * using either BSD Auth (OpenBSD) or PAM (FreeBSD / etc).
+ *
+ * \param user          The user for this challenge.
+ *
+ * \returns a status code indicating success or failure.
+ *      - 0 on success.
+ *      - non-zero on failure.
+ */
+int FN_DECL_MUST_CHECK
+fido_auth_challenge(const fido_user* user);
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
