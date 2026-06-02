1dbbb4e462a60b464cd332b5037e9dd898093218
Justin Handville <nanolith@gmail.com>
2026-06-02 00:06:02 -0400

    fido_set_user_context decl.

---
+++ include/fido/user_context.h
@@ -0,0 +1,40 @@
+/**
+ * \file fido/user_context.h
+ *
+ * \brief Data structures and methods relating to setting a user context.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <fido/error_codes.h>
+#include <fido/function_contracts.h>
+#include <fido/function_decl.h>
+#include <fido/model_assert.h>
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+/**
+ * \brief Look up the password entry for a given user and group, and set the
+ * group and user context.
+ *
+ * \note For now, we don't respect group override. TODO we will need to visit
+ * this.
+ *
+ * \param username      The username for this operation.
+ * \param groupname     The groupname for this operation.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int FN_DECL_MUST_CHECK
+fido_set_user_context(const char* username, const char* groupname);
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
