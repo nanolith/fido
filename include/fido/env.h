30f3e7cb942146dfba42db1efdeb5c3bfe69f8e8
Justin Handville <nanolith@gmail.com>
2026-06-03 00:12:18 -0400

    Added red-black tree declarations.

--- include/fido/env.h
+++ include/fido/env.h
@@ -215,6 +215,9 @@ MODEL_CONTRACT_POSTCONDITIONS_BEGIN(
     const fido_env_node* rhs)
 MODEL_CONTRACT_POSTCONDITIONS_END(fido_env_node_compare)
 
+/* Red-black tree declarations. */
+RB_PROTOTYPE(fido_env_tree, fido_env_node, node, fido_env_node_compare)
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
